cfd35c03382c86d15f16d06d8a80a96bbbbf7055
Jonas de Souza Assunção <131160367+JonasExtreme@users.noreply.github.com>
2023-07-07 11:29:28 -0300

    Add files via upload

---
+++ atividades coisa/visual studio/javascript/console.js
@@ -0,0 +1,14 @@
+//console.log("Hello World!")//
+
+const varGlobal = "Essa é uma variavel global"
+
+function mostrarvalor() {
+    let varLocal = "Essa é uma variavel local"
+
+    console.log(varGlobal)
+    console.log(varLocal)
+}
+
+//console.log(varGlobal)//
+
+mostrarvalor()
